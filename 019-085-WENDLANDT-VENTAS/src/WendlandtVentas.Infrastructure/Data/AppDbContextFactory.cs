62f2b04afb93e7c2c820935ff922178927abfcdf
DevAlanviera <alan.cordova@wendlandt.com.mx>
2025-07-22 15:52:19 -0700

    Fixed cosas

---
+++ 019-085-WENDLANDT-VENTAS/src/WendlandtVentas.Infrastructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WendlandtVentas.Infrastructure.Data
+{
+    public class AppDbContextFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public AppDbContextFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public AppDbContext CreateDbContext()
+        {
+            return _serviceProvider.GetRequiredService<AppDbContext>();
+        }
+    }
+}
